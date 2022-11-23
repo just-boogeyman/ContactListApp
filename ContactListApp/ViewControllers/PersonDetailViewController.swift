301e3f1e576de55c3b6fefa4e968ed3cc2b6baed
Aroslav Kochkin <kochkin.2328@inbox.ru>
2022-11-23 23:05:07 +0700

    add countCell

--- ContactListApp/ViewControllers/PersonDetailViewController.swift
+++ ContactListApp/ViewControllers/PersonDetailViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 class PersonDetailViewController: UITableViewController {
 
     private let persons = Person.getPersons()
+    private let countCell = 2
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,7 +28,7 @@ extension PersonDetailViewController {
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
+        return countCell
     }
     
     override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
